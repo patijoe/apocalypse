4f8a81b838d9c1fd6c2398d959b893091ad7f099
patijoe <patijoe33@gmail.com>
2024-05-19 12:32:43 +0200

    feat(server): add test to server

--- server/src/petrol-station/utils/generate-bottles.test.ts
+++ server/src/petrol-station/utils/generate-bottles.test.ts
@@ -1,18 +1,25 @@
 import { Bottle } from "../interfaces";
-import { generateBottles } from "./generate-bottles";
+import { generateBottles } from "./index";
 
 describe("generateBottles function", () => {
-  it("should generate an array with prices inside an interval", () => {
+  test("should generate an array with prices inside an interval", () => {
     const newBottles = generateBottles();
 
-    getExpectedBottlePrice(newBottles, 3.5, 4);
-    getExpectedBottlePrice(newBottles, 9, 10.5);
-    getExpectedBottlePrice(newBottles, 14, 15);
-    getExpectedBottlePrice(newBottles, 41, 42);
+    console.log("---newBottles", newBottles);
+
+    getExpectedBottlePrice(newBottles, 3.5, 4, 0);
+    getExpectedBottlePrice(newBottles, 9, 10.5, 1);
+    getExpectedBottlePrice(newBottles, 14, 15, 2);
+    getExpectedBottlePrice(newBottles, 41, 42, 3);
   });
 });
 
-const getExpectedBottlePrice = (arr: Bottle[], min: number, max: number) => {
-  expect(arr[0].price).toBeGreaterThanOrEqual(min);
-  expect(arr[0].price).toBeLessThanOrEqual(max);
+const getExpectedBottlePrice = (
+  arr: Bottle[],
+  min: number,
+  max: number,
+  index: number
+) => {
+  expect(arr[index].price).toBeGreaterThanOrEqual(min);
+  expect(arr[index].price).toBeLessThanOrEqual(max);
 };
